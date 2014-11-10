9dbaa922b5d80463c47ebeaff177d7ed23d550c3
toyama satoshi <toyamarinyon@gmail.com>
2014-11-11 07:49:49 +0900

    Add table view

--- pelot/ViewController.swift
+++ pelot/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var pickerController:UIImagePickerController = UIImagePickerController()
+    
+    var recipes = ["ビビンバ","お弁当","バナナケーキ"]
 
+    @IBOutlet weak var recipeTableView: UITableView!
+    var kCellIdentifer = "RecipeTableViewCell"
+    
+    @IBAction func addRecipeButtonTapped(sender: UIButton) {
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        pickerController.allowsEditing = true
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.recipes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.recipeTableView.dequeueReusableCellWithIdentifier(self.kCellIdentifer, forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = self.recipes[indexPath.row]
+        return cell
+    }
+    
 
 }
 
